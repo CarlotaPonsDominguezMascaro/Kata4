08a7f8c4fc58c2e4bc11fda6a0f7c8c28d10e6b2
Carlota Pons <carlotaponsdm@gmail.com>
2020-11-13 11:41:09 +0000

    Kata 4 version 1

---
+++ src/kata4/main/Kata4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata4.main;
+
+import java.util.List;
+import kata4.model.Histogram;
+import kata4.model.Mail;
+import kata4.view.HistogramDisplay;
+import kata4.view.MailHistogramBuilder;
+import kata4.view.MailListReader;
+
+/**
+ *
+ * @author carlotapons
+ */
+public class Kata4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String fileName = new String("email.txt");
+        List<Mail> mailList=MailListReader.read(fileName);
+        Histogram<String> mailHistogram = MailHistogramBuilder.build(mailList);
+        
+        
+        HistogramDisplay histo = new HistogramDisplay(mailHistogram);
+        histo.execute();
+        
+    }
+    
+}
